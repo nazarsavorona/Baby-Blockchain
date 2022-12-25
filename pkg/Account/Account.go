96357d1a1fce62c9d22d3e73173f306af720217b
CaptainShepard01 <49350081+captainshepard01@users.noreply.github.com>
2022-12-25 18:59:58 +0200

    Add account

---
+++ pkg/Account/Account.go
@@ -0,0 +1,32 @@
+package Account
+
+import (
+	"baby-blockchain/pkg/KeyPair"
+	"baby-blockchain/pkg/Signature"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"log"
+)
+
+type Account struct {
+	AccountID string
+	KeyPair   KeyPair.KeyPair
+}
+
+func GenAccount(rand io.Reader) *Account {
+	keyPair := *KeyPair.GenKeyPair(rand)
+	return &Account{AccountID: fmt.Sprint(sha256.Sum256([]byte(keyPair.ToString()))), KeyPair: keyPair}
+}
+
+func (acc *Account) signData(message string) *Signature.Signature {
+	return Signature.SignData(message, acc.KeyPair.PrivateKey)
+}
+
+func (acc *Account) ToString() string {
+	return fmt.Sprintf("{AccountID: %s; KeyPair: %v}", acc.AccountID, acc.KeyPair)
+}
+
+func (acc *Account) Print() {
+	log.Println(acc.ToString())
+}
